05717cb37b78e6fe55b2baf4933dff983efa3fa3
Marina Kuranda <mk@unisport.dk>
2019-08-21 22:11:08 +0200

    Added more lines so that the test will drag sooner

--- cypress/integration/pdp-mobile.spec.js
+++ cypress/integration/pdp-mobile.spec.js
@@ -1,8 +1,10 @@
 Cypress._.times(200, i => {
   describe(`${i}Opens PDP mobile`, () => {
-    it("Opens PDP from All Football Boots category", () => {
+    beforeEach(() => {
       cy.viewport(375, 667);
       cy.visit("/");
+    });
+    it("Opens PDP from All Football Boots category", () => {
       cy.get("#mobileMenu").click();
       cy.get(".SlideMenu > :nth-child(1) > ul > :nth-child(2)").click();
       cy.get(".SlideMenu > :nth-child(2) > ul > :nth-child(2) a").click({
@@ -28,6 +30,23 @@ Cypress._.times(200, i => {
       cy.get(".search-query").type("Adidas{enter}");
       cy.get(".product").should("be.visible");
 
+      cy.get(".image-wrap")
+        .eq(0)
+        .then(productImage => {
+          const productPageHref = productImage.parent("a").attr("href");
+          productImage.click();
+          cy.url().should("contain", productPageHref);
+          cy.get("@productTitle").then(title =>
+            cy.get("#pdp-title").should("contain", title)
+          );
+          cy.get(".fullPrice", /\d+/).should("exist");
+        });
+      // If you remove these lines from the test, the tests will slow down 
+      // at a later point, around the 190th time they are run.
+      // If you keep the lines, then the tests will start to drag at about the 130th time.
+      // It seems that the tests' slowdown is very dependant on how long the test is.
+      cy.get(".search-query").type("Nike{enter}");
+      cy.get(".product").should("be.visible");
       cy.get(".image-wrap")
         .eq(0)
         .then(productImage => {
